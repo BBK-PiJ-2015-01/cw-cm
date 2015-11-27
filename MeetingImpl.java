e47e4c122179056f0faa7949af1f6af033b8d080
USER0001 <srbaird@hotmail.com>
2015-11-27 12:17:58 +0000

    MeetingImpl tests for data and contacts

--- MeetingImpl.java
+++ MeetingImpl.java
@@ -1,5 +1,4 @@
-import java.util.Calendar;
-import java.util.Set;
+import java.util.*;
 
 /**
  *
@@ -9,7 +8,10 @@ public class MeetingImpl implements Meeting {
 
 	private final int id;
 	private Calendar date;
-
+	private Set<Contact> contacts;
+	private List<Contact> backingList = new ArrayList<>();
+	
+	private final String INVALID_CONTACTS = "The Set of contacts must not be null or empty";
 
 	public MeetingImpl(int id) {
 
@@ -24,9 +26,11 @@ public class MeetingImpl implements Meeting {
 
 	@Override
 	public Calendar getDate() {
-
-		return date;
+	
+		// Make a defensive copy
+		return date == null ? null : (Calendar) date.clone();
 	}
+
 	/**
 	* Set the date of the meeting.
 	*     
@@ -34,12 +38,42 @@ public class MeetingImpl implements Meeting {
 	*/
 	public void setDate(Calendar date) {
 
-		this.date = date;
+		// Make a defensive copy
+		this.date = date == null ? null : (Calendar) date.clone();
 	}
 
+	/**
+	* Set the contacts. The Set must not be null or empty
+	*     
+	* @param the Set of contacts
+	*/
+	public void setContacts(Set<Contact> contacts) {
+
+		if (contacts == null || contacts.isEmpty()) {
+			throw new IllegalArgumentException(INVALID_CONTACTS); 
+		}
+		//
+		//	Save the Set contacts in a List. This ensures that the 
+		//	contact Set will be the same class as supplied but
+		//	will be immutable from the perpective of this class
+		//
+		backingList = new ArrayList<>(contacts);
+		this.contacts = contacts;
+	}
+	/**
+	* Get the contacts. This will return a reference to the Set used in the
+	* setter with the same content. If this pointer has been used elsewhere
+	* then it will also reflect the values at the time of the setter.
+	*/
 	@Override
 	public Set<Contact> getContacts() {
-		throw new UnsupportedOperationException("Not implemented"); 
-	}
-    
+
+		if (contacts == null) {
+			return null;
+		}
+		//	Return a defensive copy
+		contacts.clear();
+		contacts.addAll(backingList);
+		return contacts;
+	}  
 }
--- TestMeetingImpl.java
+++ TestMeetingImpl.java
@@ -39,6 +39,174 @@ public class TestMeetingImpl {
 		Calendar resultDate = instance.getDate();
 		assertEquals(expectedDate, resultDate);
 	}
+
+	@Test
+	public void setDateWithNonNullTest() {
+	
+		Calendar expectedDate = Calendar.getInstance();
+		instance.setDate(expectedDate);
+		Calendar resultDate = instance.getDate();
+		assertEquals(expectedDate, resultDate);
+	}
+
+	@Test
+	public void getDateImmutabilityTest() {
+	
+		//
+		//	Create 2 equal dates
+		//
+		Calendar expectedDate = Calendar.getInstance();
+		Calendar copyDate = (Calendar) expectedDate.clone();
+		assertEquals(expectedDate, copyDate);
+		//
+		//	Set the date using the copy
+		//
+		instance.setDate(copyDate);
+		Calendar resultDate = instance.getDate();
+		assertEquals(expectedDate, resultDate);
+		//
+		//	Clear the returned date
+		//
+		resultDate.clear();
+		//
+		resultDate = instance.getDate();
+		assertEquals(expectedDate, resultDate);
+	}
+
+	@Test
+	public void setDateImmutabilityTest() {
+	
+		//
+		//	Create 2 equal dates
+		//
+		Calendar expectedDate = Calendar.getInstance();
+		Calendar copyDate = (Calendar) expectedDate.clone();
+		assertEquals(expectedDate, copyDate);
+		//
+		//	Set the date using the copy
+		//
+		instance.setDate(copyDate);
+		Calendar resultDate = instance.getDate();
+		assertEquals(expectedDate, resultDate);
+		//
+		//	Clear the date used in the setter 
+		//
+		copyDate.clear();
+		//
+		resultDate = instance.getDate();
+		assertEquals(expectedDate, resultDate);
+	}
+
+	@Test(expected=IllegalArgumentException.class)
+	public void setContactsWithNullValueTest() {
+		
+		instance.setContacts(null);
+	}
+
+
+	@Test(expected=IllegalArgumentException.class)
+	public void setContactsWithEmptyValueTest() {
+		
+		Set<Contact> contacts = new TreeSet<>();
+		instance.setContacts(contacts);
+	}
+
+	@Test
+	public void setContactsTest() {
+		
+		Set<Contact> expectedContacts = new TreeSet<>();
+		expectedContacts.add(new ContactImpl());
+		instance.setContacts(expectedContacts);
+		Set<Contact> resultContacts = instance.getContacts();
+		assertArrayEquals(expectedContacts.toArray(), resultContacts.toArray());	
+	}
+
+	@Test
+	public void getContactsImmutabilityTest() {
+	
+		//
+		//	Create 2 equal contacts Sets
+		//
+		Set<Contact> expectedContacts = new TreeSet<>();
+		Set<Contact> copyContacts = new TreeSet<>();
+		Contact singleContact = new ContactImpl();
+		expectedContacts.add(singleContact);
+		copyContacts.add(singleContact);
+		assertArrayEquals(expectedContacts.toArray(), copyContacts.toArray());
+		//
+		//	Set the contacts using the copy
+		//
+		instance.setContacts(copyContacts);
+		Set<Contact> resultContacts = instance.getContacts();
+		assertArrayEquals(expectedContacts.toArray(), resultContacts.toArray());	
+		//
+		//	Clear the returned contacts
+		//
+		resultContacts.clear();
+		//
+		resultContacts = instance.getContacts();
+		assertArrayEquals(expectedContacts.toArray(), resultContacts.toArray());	
+	}
+	
+	@Test
+	public void setContactsImmutabilityTest() {
+	
+		//
+		//	Create 2 equal contacts Sets
+		//
+		Set<Contact> expectedContacts = new TreeSet<>();
+		Set<Contact> copyContacts = new TreeSet<>();
+		Contact singleContact = new ContactImpl();
+		expectedContacts.add(singleContact);
+		copyContacts.add(singleContact);
+		assertArrayEquals(expectedContacts.toArray(), copyContacts.toArray());
+		//
+		//	Set the contacts using the copy
+		//
+		instance.setContacts(copyContacts);
+		Set<Contact> resultContacts = instance.getContacts();
+		assertArrayEquals(expectedContacts.toArray(), resultContacts.toArray());
+		//
+		//	Clear the contacts used in the setter 
+		//
+		copyContacts.clear();
+		//
+		resultContacts = instance.getContacts();
+		assertArrayEquals(expectedContacts.toArray(), resultContacts.toArray());
+	}
+
+
+
+
+
+	private class ContactImpl implements Contact, Comparable<Contact> {
+
+		@Override
+		public int getId() {
+		throw new UnsupportedOperationException("Not implemented.");
+		}
+
+		@Override
+		public String getName() {
+		throw new UnsupportedOperationException("Not implemented.");
+		}
+
+		@Override
+		public String getNotes() {
+		throw new UnsupportedOperationException("Not implemented.");
+		}
+
+		@Override
+		public void addNotes(String note) {
+		throw new UnsupportedOperationException("Not implemented.");
+		}
+
+		@Override
+		public int compareTo(Contact o) {
+			
+			return o == null ? -1 : hashCode() - o.hashCode();
+		}
+	}
 }
 
 
