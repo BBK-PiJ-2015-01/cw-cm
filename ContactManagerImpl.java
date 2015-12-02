50b0c04b5626a85cbf3f23ba88a79e3c517c8cb9
USER0001 <srbaird@hotmail.com>
2015-12-02 18:40:03 +0000

    Coded for new Past Meeting test

--- ContactManagerImpl.java
+++ ContactManagerImpl.java
@@ -81,7 +81,7 @@ public class ContactManagerImpl implements ContactManager {
 		if (contacts == null || date == null || text == null) {
 			throw new NullPointerException(NULL_PARAM_MSG);
 		}
-		if (contacts.isEmpty() ) {
+		if (contacts.isEmpty() || !this.contacts.containsAll(contacts)) {
 			throw new IllegalArgumentException(INVALID_PARAM_MSG);
 		}
 	}
