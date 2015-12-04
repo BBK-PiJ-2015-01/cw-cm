79af0801e3c321f7e234fce44b35ad0e79b89389
USER0001 <srbaird@hotmail.com>
2015-12-04 18:39:54 +0000

    updateContacts() code for SerializableContactManagerModel

--- SerializableContactManagerModel.java
+++ SerializableContactManagerModel.java
@@ -57,6 +57,9 @@ public class SerializableContactManagerModel implements ContactManagerModel {
 
 	@Override
 	public void updateContact(ModelContact contact) {
+		if (contact == null) {
+			throw new NullPointerException(NULL_PARAM_MSG);
+		}
 	throw new UnsupportedOperationException("Unsupported operation."); 
 	}
 
