b312efcf1b2cbfdab1b0059d2d77626bbe170008
USER0001 <srbaird@hotmail.com>
2015-12-04 17:32:50 +0000

    getContact() code for SerializableContactManagerModel

--- SerializableContactManagerModel.java
+++ SerializableContactManagerModel.java
@@ -29,7 +29,8 @@ public class SerializableContactManagerModel implements ContactManagerModel {
 
 	@Override
 	public Contact getContact(int id) {
-	throw new UnsupportedOperationException("Unsupported operation."); 
+
+		return null;
 	}
 
 	@Override
