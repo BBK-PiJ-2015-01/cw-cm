e7d46eb2ef1d58b64cc2622fa9783b1df562e4e1
USER0001 <srbaird@hotmail.com>
2015-12-05 12:03:44 +0000

    Added SerializableFilePersistenceUnit

---
+++ SerializableFilePersistenceUnit.java
@@ -0,0 +1,23 @@
+**
+ *
+ * @author sbaird02
+ */
+public class SerializableFilePersistenceUnit implements PersistenceUnit {
+
+	@Override
+	public void load() throws PersistenceUnitException {
+	throw new UnsupportedOperationException("Unsupported operation.");
+	}
+
+	@Override
+	public void commit() throws PersistenceUnitException {
+	throw new UnsupportedOperationException("Unsupported operation.");
+	}
+
+	@Override
+	public ContactManagerModel getModel() {
+	throw new UnsupportedOperationException("Unsupported operation.");
+	}
+
+}
+
