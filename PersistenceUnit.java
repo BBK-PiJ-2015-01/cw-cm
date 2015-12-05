61a41ca2aae1fe92fe96078a9862b10582be90cc
USER0001 <srbaird@hotmail.com>
2015-12-05 12:01:31 +0000

    Added PersistenceUnit

---
+++ PersistenceUnit.java
@@ -0,0 +1,29 @@
+/**
+ *
+ * @author sbaird02
+ */
+public interface PersistenceUnit {
+
+    /**
+     * Load the model from the persistence unit.
+     *
+     * @throws PersistenceUnitException
+     */
+    void load() throws PersistenceUnitException;
+
+    /**
+     * Commit updates made to the model to the persistence unit.
+     *
+     * @throws PersistenceUnitException
+     */
+    void commit() throws PersistenceUnitException;
+
+    /**
+     * Return the ContactManager model
+     *
+     * @return an instance of the ContactManagerModel
+     */
+    ContactManagerModel getModel();
+
+}
+
